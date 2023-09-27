fbd7eeda7afd9aa8b90395ad9f7a491a98f24151
unknown <dubolarime@gmail.com>
2023-09-27 11:07:15 +0100

    init project infrastructure, maven, spring boot and dependencies

---
+++ src/main/java/com/tsb/lbl/LblApplication.java
@@ -0,0 +1,13 @@
+package com.tsb.lbl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LblApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LblApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsb/lbl/LblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsb.lbl;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LblApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
