b4b96a2c6064a121b66f09572f3bccf089090b00
unknown <dubolarime@gmail.com>
2023-09-30 15:19:24 +0100

    masked infura key

--- src/main/java/com/tsb/lbl/AppConstants.java
+++ src/main/java/com/tsb/lbl/AppConstants.java
@@ -2,7 +2,7 @@ package com.tsb.lbl;
 
 public interface AppConstants {
     //Infura network address and credentials token
-    String INFURA_WS_ADDRESS = "wss://mainnet.infura.io/ws/v3/0211c89b457a466389007764d34eb5e3";
+    String INFURA_WS_ADDRESS = "wss://mainnet.infura.io/ws/v3/<masked>";
     //Deployed Sandbox contract address: https://etherscan.io/address/0x3845badade8e6dff049820680d1f14bd3903a5d0
     String CONTRACT_ADDRESS = "0x3845badAde8e6dFF049820680d1F14bD3903a5d0";
 }
